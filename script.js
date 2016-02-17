90e3bfdadc3f210e126bcbf19964a06e75956bcd
Klajdi Bejko <kbejko@gmail.com>
2016-02-17 13:16:02 -0500

    Add balloons, scss, balloon interaction

--- script.js
+++ script.js
@@ -3,8 +3,15 @@ $(document).ready(function(){
     $(this).toggleClass('flap-open')
     $('.card').toggleClass('show')
     $('.card').removeClass('show-card')
+    $('.balloon').clone().appendTo('envelope')
   })
   $('.card').on('click', function(){
     $(this).toggleClass('show-card')
+    $('.balloon').addClass('balloon-show')
+  })
+  $('.balloon').each(function(){
+    $(this).on('click', function(){
+      $(this).addClass('pop')
+    })
   })
 })
